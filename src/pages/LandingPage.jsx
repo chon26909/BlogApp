980fa00476abec6e8e87108d5249c243fe20ce0b
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-04 14:05:11 +0700

    add carousel in landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import HotContent from '../components/HotContent';
+import CarouselSlider from "../components/CarouselSlider";
 
 const LandingPage = () => {
     return (
         <div>
+            <CarouselSlider/>
             <HotContent/>
         </div>
     )
