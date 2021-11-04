16d3a9c4331c26ef95d931a70d53f3f7756b0e30
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-04 11:46:58 +0700

    mock data

---
+++ src/data/buzz.js
@@ -0,0 +1,6 @@
+export default buzz = [
+    {
+        title: "",
+        image: ""
+    }
+]
---
+++ src/data/game_show.js
@@ -0,0 +1,28 @@
+const gameshow = {
+    title: "Thailand Game Show",
+    subtitle: "จับทุกความเคลื่อนไหวในวงการเกม",
+    list: [
+        {
+            title: "Harry Potter: Wizards Unite จะยุติการให้บริการ ภายในเดือนมกราคม ปี ค.ศ. 2022",
+            image: "https://www.beartai.com/wp-content/uploads/2021/11/206130685_1223487054740403_1087003980402066567_n-370x247.jpg"
+        },
+        {
+            title: "Netflix เพิ่มหมวด Netflix Games เข้ามา มีถึง 5 เกมให้ได้เล่นกันในช่วงแรก",
+            image: "https://www.beartai.com/wp-content/uploads/2021/11/Android_Collage_1920x1080__UCAN_En-370x247.jpg"
+        },
+        {
+            title: "PlatinumGames ประกาศเลื่อนวางจำหน่าย Sol Cresta",
+            image: "https://www.beartai.com/wp-content/uploads/2021/11/Sol-Cresta_2021_08-05-21_011-1155x770.jpg"
+        },
+        {
+            title: "เปิดวิธีรับธีม K PLUS ลาย RoV – Glorious Ruler สุดเท่ ง่าย ๆ แค่ 3 ขั้นตอน",
+            image: "https://www.beartai.com/wp-content/uploads/2021/11/rov-kbank-370x247.jpg"
+        },
+        {
+            title: "Ghost Recon: Breakpoint จะเปิดให้ทดลองเล่นฟรีในสัปดาห์นี้",
+            image: "https://www.beartai.com/wp-content/uploads/2021/11/FDNCwnBWUAUgr8Z-370x247.jpg"
+        }
+    ]
+};
+
+export default gameshow;
